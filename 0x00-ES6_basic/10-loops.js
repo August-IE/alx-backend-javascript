144cf2950ed56eebfc30460f1de4bcda0435c3d1
August-IE <aeromosele9@gmail.com>
2024-04-09 07:56:39 +0000

    task 10 modified

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const result = [];
-  for (const idx in array) {
-    if (Object.prototype.hasOwnProperty.call(array, idx)) {
-      const value = appendString + array[idx];
-      result.push(value);
-    }
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
-  return result;
+  return newArray;
 }
