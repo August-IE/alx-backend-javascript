b1ea86bc8386b5ecdf57798bc96b48829de06840
August-IE <aeromosele9@gmail.com>
2024-04-11 11:27:21 +0000

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,25 +2,7 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     this._name = name;
     this._length = length;
-    this._students = Array.isArray(students) ? students : [];
-
-    // Type verification for name
-    if (typeof this._name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    // Type verification for length
-    if (typeof this._length !== 'number' || Number.isNaN(this._length)) {
-      throw new TypeError('Length must be a number');
-    }
-    // Type verification for students
-    if (!Array.isArray(this._students)) {
-      throw new TypeError('Students must be an array');
-    }
-    for (const student of this._students) {
-      if (typeof student !== 'string') {
-        throw new TypeError('Each student must be a string');
-      }
-    }
+    this._students = students;
   }
 
   // Getter and setter for name
@@ -53,14 +35,10 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (!Array.isArray(value)) {
-      throw new TypeError('Students must be an array');
+    if (typeof value instanceof Array) {
+      this._students = value;
+    } else {
+      throw new TypeError('Students must be an Array');
     }
-    for (const student of value) {
-      if (typeof student !== 'string') {
-        throw new TypeError('Each student must be a string');
-      }
-    }
-    this._students = value;
   }
 }
