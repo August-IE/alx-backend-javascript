b93c3efe8b8aaaa508f8e18dfe38111d517ba7c5
August-IE <aeromosele9@gmail.com>
2024-04-11 23:25:40 +0000

    Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,29 @@
+export default class Airport {
+// Initializes an Airport
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  // Implementing getter & setter methods
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  // Gets a string representation of the Airpirt
+  toString() {
+    return `[${this._code}]`;
+  }
+}
