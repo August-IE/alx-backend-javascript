935ff5fd1f4697c9139e9a8aeb7e798c3dd89482
August-IE <aeromosele9@gmail.com>
2024-04-16 10:58:10 +0000

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,18 @@
+export default function cleanSet(set, startString) {
+  if (!(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+
+  const parts = [];
+
+  for (const value of set) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+      if (valueSubStr) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+
+  return parts.join('-');
+}
