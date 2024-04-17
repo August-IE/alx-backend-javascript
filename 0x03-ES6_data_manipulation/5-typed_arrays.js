ccce83d0b2787a65f980ea23f800f5bc56aebfb2
August-IE <aeromosele9@gmail.com>
2024-04-17 17:16:40 +0000

    Typed Arrays failed execution resolved

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) {
-    throw new Error('Position is outside the range of the array');
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
   }
 
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
 
-  view.setUint8(position, value);
+  view.setInt8(position, value);
 
   return view;
 }
